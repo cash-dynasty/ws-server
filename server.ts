8c7817d18c2752dae323afaacd81e6527314fafa
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2023-10-05 20:58:23 +0200

    cors update

--- server.ts
+++ server.ts
@@ -1,39 +1,47 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import {Server} from "socket.io";
+import express from "express";
+import { Server } from "socket.io";
+import axios from "axios";
+import cors from 'cors'
 
-// const io = new Server(server, {
-//   cors: {
-//     origin: "*",
-//     methods: ["GET", "POST"],
-//   },
-// });
+const app = express();
 
-// io.on("connection", (socket) => {
-//   console.log("a user connected", socket.client.id, 'elo' );
-//
-//   socket.on("chat", async (props) => {
-//     console.log("message: " + JSON.stringify(props));
-//
-//     await axios
-//         .post("https://cashdynasty.pl/api/chat", {
-//           message: props.message,
-//           userId: props.userId,
-//           conversation: props.conversation,
-//         })
-//         .then((res) => {
-//           io.emit("chat", props.message);
-//         });
-//   });
-// });
+app.use(cors())
+app.use(express.json())
 
-const io = new Server(3001)
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true
+  },
+});
 
-io.on('connection', (socket) => {
-        socket.emit('hello', 'world'
-        )
-        socket.on('howdy', (arg) => {
-            console.log(arg)
-        })
-    }
-)
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.client.id );
+
+  socket.on("chat", async (props) => {
+    console.log("message: " + JSON.stringify(props));
+
+    await axios
+      .post("https://cashdynasty.pl/api/chat", {
+        message: props.message,
+        userId: props.userId,
+        conversation: props.conversation,
+      })
+      .then((res) => {
+        io.emit("chat", props.message);
+      });
+  });
+});
+
+server.listen(3001, () => {
+  console.log(`server running at port 3001`);
+});
