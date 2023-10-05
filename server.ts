066858db52532d2b9b0efa9c29e943a84cc08d0c
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2023-10-05 21:04:26 +0200

    cors update

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import express from "express";
-import { Server } from "socket.io";
+import {Server} from "socket.io";
 import axios from "axios";
 import cors from 'cors'
+import {createServer} from 'http'
 
 const app = express();
 
@@ -12,36 +13,36 @@ app.use(express.json())
 
 const server = createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true
-  },
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true
+    },
 });
 
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+    res.send("<h1>Hello world</h1>");
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected", socket.client.id );
+    console.log("a user connected", socket.client.id);
 
-  socket.on("chat", async (props) => {
-    console.log("message: " + JSON.stringify(props));
+    socket.on("chat", async (props) => {
+        console.log("message: " + JSON.stringify(props));
 
-    await axios
-      .post("https://cashdynasty.pl/api/chat", {
-        message: props.message,
-        userId: props.userId,
-        conversation: props.conversation,
-      })
-      .then((res) => {
-        io.emit("chat", props.message);
-      });
-  });
+        await axios
+            .post("https://cashdynasty.pl/api/chat", {
+                message: props.message,
+                userId: props.userId,
+                conversation: props.conversation,
+            })
+            .then((res) => {
+                io.emit("chat", props.message);
+            });
+    });
 });
 
 server.listen(3001, () => {
-  console.log(`server running at port 3001`);
+    console.log(`server running at port 3001`);
 });
