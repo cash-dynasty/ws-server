85607bb72a9b501f15c229491e15e312b422e824
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2023-10-05 20:11:20 +0200

    cors update

--- server.ts
+++ server.ts
@@ -16,24 +16,6 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected", socket.client.id, 'elo' );
-
-  socket.on("chat", async (props) => {
-    console.log("message: " + JSON.stringify(props));
-
-    await axios
-      .post("https://cashdynasty.pl/api/chat", {
-        message: props.message,
-        userId: props.userId,
-        conversation: props.conversation,
-      })
-      .then((res) => {
-        io.emit("chat", props.message);
-      });
-  });
-});
-
 const server = app.listen(3001, () => {
   console.log(`server running at port 3001`);
 });
@@ -44,4 +26,22 @@ const io = new Server(server, {
     origin: "https://cashdynasty.pl",
     methods: ["GET", "POST"],
   },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.client.id, 'elo' );
+
+  socket.on("chat", async (props) => {
+    console.log("message: " + JSON.stringify(props));
+
+    await axios
+        .post("https://cashdynasty.pl/api/chat", {
+          message: props.message,
+          userId: props.userId,
+          conversation: props.conversation,
+        })
+        .then((res) => {
+          io.emit("chat", props.message);
+        });
+  });
 });
