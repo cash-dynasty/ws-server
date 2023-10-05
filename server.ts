398bce716ee1f9825f1dfb00a85181f81f9e6b18
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2023-10-05 20:31:22 +0200

    cors update

--- server.ts
+++ server.ts
@@ -1,47 +1,39 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import express from "express";
-import { Server } from "socket.io";
-import axios from "axios";
-import cors from 'cors'
+import {Server} from "socket.io";
 
-const app = express();
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
-app.use(cors())
-app.use(express.json())
+// io.on("connection", (socket) => {
+//   console.log("a user connected", socket.client.id, 'elo' );
+//
+//   socket.on("chat", async (props) => {
+//     console.log("message: " + JSON.stringify(props));
+//
+//     await axios
+//         .post("https://cashdynasty.pl/api/chat", {
+//           message: props.message,
+//           userId: props.userId,
+//           conversation: props.conversation,
+//         })
+//         .then((res) => {
+//           io.emit("chat", props.message);
+//         });
+//   });
+// });
 
+const io = new Server(3001)
 
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
-});
-
-const server = app.listen(3001, () => {
-  console.log(`server running at port 3001`);
-});
-
-
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("a user connected", socket.client.id, 'elo' );
-
-  socket.on("chat", async (props) => {
-    console.log("message: " + JSON.stringify(props));
-
-    await axios
-        .post("https://cashdynasty.pl/api/chat", {
-          message: props.message,
-          userId: props.userId,
-          conversation: props.conversation,
+io.on('connection', (socket) => {
+        socket.emit('hello', 'world'
+        )
+        socket.on('howdy', (arg) => {
+            console.log(arg)
         })
-        .then((res) => {
-          io.emit("chat", props.message);
-        });
-  });
-});
+    }
+)
