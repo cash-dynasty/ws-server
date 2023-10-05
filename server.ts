881547801a00d9dbf1bf8bc5a6636c3779ff7da0
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2023-10-05 18:13:45 +0200

    update

--- server.ts
+++ server.ts
@@ -13,9 +13,9 @@ const io = new Server(server, {
   },
 });
 
-// app.get("/", (req, res) => {
-//   res.send("<h1>Hello world</h1>");
-// });
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.client.id );
