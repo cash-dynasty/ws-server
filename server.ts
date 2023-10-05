519d6a6990967fda31fb5aaebcab9ff3fd6849f7
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2023-10-06 00:55:29 +0200

    cors update

--- server.ts
+++ server.ts
@@ -10,25 +10,25 @@ const app = express();
 // const origin = "https://cashdynasty.pl"
 // const origin = "*"
 
-// app.use(cors({
-//     origin: '*',
-//     methods: ["GET", "POST"],
-// }))
-// app.use(express.json())
-//
-//
-// app.get("/", (req, res) => {
-//     res.send("<h1>Hello world</h1>");
-// });
-//
-// app.post("/test", (req, res) => {
-//     res.send({message: "test"});
-// });
-//
-//
-// app.listen(3001, () => {
-//     console.log(`server running at port 3001`);
-// });
+app.use(cors({
+    origin: '*',
+    methods: ["GET", "POST"],
+}))
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello world</h1>");
+});
+
+app.post("/test", (req, res) => {
+    res.send({message: "test"});
+});
+
+
+app.listen(3001, () => {
+    console.log(`server running at port 3001`);
+});
 
 
 const httpServer = createServer(app);
