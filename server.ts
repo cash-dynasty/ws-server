509bf10797d6e75815cee16919db79831e042ebf
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2023-10-06 01:01:28 +0200

    cors update

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import express from "express";
+import axios from "axios";
 import cors from "cors";
 import {Server} from "socket.io";
 import {createServer} from "http";
@@ -27,19 +28,20 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
     console.log("a user connected", socket.client.id);
-    // socket.on("chat", async (props) => {
-    //     console.log("message: " + JSON.stringify(props));
-    //
-    //     // await axios
-    //     //     .post("https://cashdynasty.pl/api/chat", {
-    //     //       message: props.message,
-    //     //       userId: props.userId,
-    //     //       conversation: props.conversation,
-    //     //     })
-    //     //     .then((res) => {
-    //     io.emit("chat", props.message);
-    //     // });
-    // });
+    socket.on("chat", async (props) => {
+        console.log("message: " + JSON.stringify(props));
+
+        await axios
+            .post("https://cashdynasty.pl/api/chat", {
+                message: props.message,
+                userId: props.userId,
+                conversation: props.conversation,
+            })
+            .then((res) => {
+                console.log(res);
+                io.emit("chat", props.message);
+            });
+    });
 });
 
 app.get("/", (req, res) => {
@@ -54,8 +56,6 @@ app.post("/test", (req, res) => {
 app.listen(3001, () => {
     console.log(`server running at port 3001`);
 });
-
-
 
 
 //
