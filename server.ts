9407426c98d95f0c1143e0416ea6c243cd87cf6a
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2023-10-05 23:50:59 +0200

    cors update

--- server.ts
+++ server.ts
@@ -1,9 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import express from "express";
-import {Server} from "socket.io";
-import cors from 'cors'
-import {createServer} from "http";
 
 const app = express();
 
@@ -11,11 +8,15 @@ const app = express();
 // const origin = "*"
 
 // app.use(cors())
-// app.use(express.json())
+app.use(express.json())
 
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello world</h1>");
+});
+
+app.post("/test", (req, res) => {
+    res.send({message: "test"});
 });
 
 
@@ -24,18 +25,19 @@ app.listen(3001, () => {
 });
 
 
-const httpServer = createServer(app);
-
-const io = new Server(httpServer, {
-    cors: {
-        origin: ['https://cashdynasty.pl', 'http://localhost:3000'],
-        methods: ["GET", "POST"],
-        credentials: false,
-    },
-});
-
-io.on("connection", (socket) => {
-    console.log("a user connected", socket.client.id);
+// const httpServer = createServer(app);
+//
+// const io = new Server(httpServer, {
+//     cors: {
+//         origin: ['https://cashdynasty.pl', 'http://localhost:3000'],
+//         methods: ["GET", "POST"],
+//         credentials: false,
+//     },
+// });
+//
+// io.on("connection", (socket) => {
+//     console.log("a user connected", socket.client.id);
+// });
 //
 //     socket.on("chat", async (props) => {
 //         console.log("message: " + JSON.stringify(props));
@@ -50,4 +52,3 @@ io.on("connection", (socket) => {
 //         io.emit("chat", props.message);
 //         // });
 //     });
-});
